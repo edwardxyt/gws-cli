8a589769c50d6223a4eadd9767121a32c321762c
夏宇霆Pro <edward56833517@gmail.com>
2019-08-01 19:56:09 +0800

    Initial commit

--- src/simple/config/webpack.production.config.js
+++ src/simple/config/webpack.production.config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const HappyPack = require("happypack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -33,7 +33,7 @@ let initConfig = async () => {
         resolve: app_config.resolve,
         externals: app_config.externals,
         devtool: app_config.debug
-            ? "cheap-module-eval-source-map"  // 1.09 MiB
+            ? "cheap-module-eval-source-map" // 1.09 MiB
             : false,
         module: {
             rules: [
@@ -41,7 +41,12 @@ let initConfig = async () => {
                     test: /\.(js|jsx)$/,
                     use: "happypack/loader?id=jsx",
                     // exclude: [app_config.node_module_dir],
-                    include: [app_config.src, `${app_config.node_module_dir}/@edwardxyt/gws-components`]
+                    include: [
+                        app_config.src,
+                        `${
+                            app_config.node_module_dir
+                        }/@edwardxyt/gws-components`
+                    ]
                 },
                 {
                     test: /\.hbs/,
@@ -67,7 +72,12 @@ let initConfig = async () => {
                         },
                         "less-loader"
                     ],
-                    include: [app_config.src, `${app_config.node_module_dir}/@edwardxyt/gws-components`]
+                    include: [
+                        app_config.src,
+                        `${
+                            app_config.node_module_dir
+                        }/@edwardxyt/gws-components`
+                    ]
                 },
                 {
                     test: /\.css$/,
@@ -97,7 +107,12 @@ let initConfig = async () => {
                         }
                     ],
                     // exclude: [app_config.node_module_dir],
-                    include: [app_config.src, `${app_config.node_module_dir}/@edwardxyt/gws-components`]
+                    include: [
+                        app_config.src,
+                        `${
+                            app_config.node_module_dir
+                        }/@edwardxyt/gws-components`
+                    ]
                 }
             ]
         },
@@ -106,18 +121,25 @@ let initConfig = async () => {
             // 	analyzerPort: 27003,
             // }),
             new CopyPlugin([
-                { from: `${app_config.rootDir}/README.md`, to: `${app_config.dist}/${app_config.entry}/` },
+                {
+                    from: `${app_config.rootDir}/README.md`,
+                    to: `${app_config.dist}/${app_config.entry}/`
+                }
             ]),
-            new webpack.ProvidePlugin({  //这些变量不必再import了
-                React: 'react',
-                ReactDOM: 'react-dom',
-                Component: ['react','Component'] // 导出react模块中的Component
+            new webpack.ProvidePlugin({
+                //这些变量不必再import了
+                React: "react",
+                ReactDOM: "react-dom",
+                Component: ["react", "Component"] // 导出react模块中的Component
             }),
             new UglifyJsPlugin({
                 cache: true,
                 parallel: true, // 并行
                 sourceMap: app_config.debug, // set to true if you want JS source maps
-                include: [`${app_config.src}/${app_config.entry}/`, `${app_config.node_module_dir}/@edwardxyt/gws-components`]
+                include: [
+                    `${app_config.src}/${app_config.entry}/`,
+                    `${app_config.node_module_dir}/@edwardxyt/gws-components`
+                ]
             }),
             new webpack.DllReferencePlugin({
                 manifest: path.join(
@@ -128,8 +150,8 @@ let initConfig = async () => {
             }),
             new HappyPack({
                 id: "jsx",
-                threads: 4,
-                debug: true,
+                // threads: 4,
+                // debug: true,
                 loaders: [
                     {
                         loader: "babel-loader",
@@ -138,16 +160,22 @@ let initConfig = async () => {
                                 [
                                     "@babel/preset-env",
                                     {
-                                        "targets": {
-                                            "esmodules": true
+                                        targets: {
+                                            esmodules: true
                                         }
                                     }
                                 ],
-                                '@babel/preset-react'
+                                "@babel/preset-react"
                             ],
                             plugins: [
-                                ["@babel/plugin-proposal-decorators", { "legacy": true }],
-                                ["@babel/plugin-proposal-class-properties", { "loose" : true }],
+                                [
+                                    "@babel/plugin-proposal-decorators",
+                                    {legacy: true}
+                                ],
+                                [
+                                    "@babel/plugin-proposal-class-properties",
+                                    {loose: true}
+                                ],
                                 "@babel/plugin-transform-runtime",
                                 "@babel/plugin-syntax-dynamic-import",
                                 app_config.antd
@@ -156,18 +184,20 @@ let initConfig = async () => {
                     }
                 ]
             }),
-            new webpack.optimize.ModuleConcatenationPlugin(),  // 启用作用域提升，作用是让代码文件更小、运行的更快
+            new webpack.optimize.ModuleConcatenationPlugin(), // 启用作用域提升，作用是让代码文件更小、运行的更快
             new webpack.DefinePlugin(app_config.inject),
-            new webpack.BannerPlugin(`xs build at ${Date.now()}`),   // 打包后在.js/.css页头的时间
-            new OptimizeCSSAssetsPlugin({  // 用于优化或者压缩CSS资源
+            new webpack.BannerPlugin(`xs build at ${Date.now()}`), // 打包后在.js/.css页头的时间
+            new OptimizeCSSAssetsPlugin({
+                // 用于优化或者压缩CSS资源
                 assetNameRegExp: /\.optimize\.css$/g,
-                cssProcessor: require('cssnano'),
+                cssProcessor: require("cssnano"),
                 cssProcessorPluginOptions: {
-                    preset: ['default', { discardComments: { removeAll: true } }],
+                    preset: ["default", {discardComments: {removeAll: true}}]
                 },
                 canPrint: true
             }),
-            new MiniCssExtractPlugin({   // 分离css
+            new MiniCssExtractPlugin({
+                // 分离css
                 filename: "styles/[name].[contenthash].css",
                 chunkFilename: "styles/[id].[contenthash].css"
             }),
