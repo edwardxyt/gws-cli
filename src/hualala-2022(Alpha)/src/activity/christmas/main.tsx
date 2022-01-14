683b02f2d163c2bb23fd30a7565fa1d958c1eaef
夏宇霆Pro <edward56833517@gmail.com>
2022-01-14 16:14:47 +0800

    2022 no copy

--- src/hualala-2022(Alpha)/src/activity/christmas/main.tsx
+++ src/hualala-2022(Alpha)/src/activity/christmas/main.tsx
@@ -1,30 +1,30 @@
 import * as _ from 'lodash';
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import './index.less'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.less';
 
-import avatar from './asset/avatar.png'
+import avatar from './asset/avatar.png';
 
 // 热启动
 if (module.hot) {
-    module.hot.accept()
+    module.hot.accept();
 }
 
-var a =10
+var a = 10;
 
 // PWA
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        navigator.serviceWorker.register('/service-worker.js').then((registration) => {
             console.log('SW registered: ', registration);
-        }).catch(registrationError => {
+        }).catch((registrationError) => {
             console.log('SW registration failed: ', registrationError);
         });
     });
 }
 
-interface HelloProps { 
-    compiler: string; 
+interface HelloProps {
+    compiler: string;
     framework: string;
 }
 
@@ -35,17 +35,16 @@ export class Hello extends React.Component<HelloProps, {}> {
                 <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>
                 <img src={ avatar } alt="" />
             </>
-        )
+        );
     }
 }
 
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("main")
+    document.getElementById('main'),
 );
 
-
-// interface Person { 
+// interface Person {
 //     firstName: string;
 //     lastName: string;
 // }
