57fe5110fa120cdf0d60d4b48bd56cc67c8d6341
夏宇霆 <56833517@qq.com>
2022-09-05 17:14:23 +0800

    fix: workspace.ts 全屏时，实时表不刷新问题

--- src/2022(Alpha)/config/project.js
+++ src/2022(Alpha)/config/project.js
@@ -34,17 +34,43 @@ module.exports = {
           cdn_path: '/',
         },
         development: {
-          api_path: '//:bizdev.aibao.com',
+          api_path: '//:bizdev.xiayuting.com',
           console: false,
           cdn_path: '/',
         },
         test: {
-          api_path: '//:bizapitest.aibao.com',
+          api_path: '//:bizapitest.xiayuting.com',
           console: false,
           cdn_path: '/',
         },
         production: {
-          api_path: '//:biz.aibao.com',
+          api_path: '//:biz.xiayuting.com',
+          console: false,
+          cdn_path: '/',
+        },
+      },
+    },
+  },
+  demo: {
+    modules: {
+      env: {
+        mock: {
+          api_path: '123123',
+          console: false,
+          cdn_path: '/',
+        },
+        development: {
+          api_path: '//:bizdev.xiayuting.com',
+          console: false,
+          cdn_path: '/',
+        },
+        test: {
+          api_path: '//:bizapitest.xiayuting.com',
+          console: false,
+          cdn_path: '/',
+        },
+        production: {
+          api_path: '//:biz.xiayuting.com',
           console: false,
           cdn_path: '/',
         },
